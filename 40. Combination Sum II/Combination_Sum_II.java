02c5f7d77a545af6fc17cad1c6d46fe7c9da676c
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-09-05 10:19:41 +0530

    medium

---
+++ 40. Combination Sum II/Combination_Sum_II.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Comnination_Sun_II{
+
+    public List<List<Integer>> combinationSum2(int []nums,int target){
+        List<List<Integer>> res =new ArrayList<>();
+        solve(0,nums,target,res,new ArrayList<>());
+        return res;
+    }
+
+    private void solve(int index,int []nums,int target,List<List<Integer>> res,List<Integer> curr){
+        if(target==0){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if(target<0) return;
+
+        for(int i=index;i<nums.length;i++){
+            curr.add(nums[i]);
+            solve(i+1, nums, target-nums[i], res, curr);
+            curr.remove(curr.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Comnination_Sun_II obj=new Comnination_Sun_II();
+        int []arr={10,1,2,7,6,1,5};
+        int target=7;
+        List<List<Integer>> res=obj.combinationSum2(arr,target);
+        System.out.println(res);
+    }
+}
