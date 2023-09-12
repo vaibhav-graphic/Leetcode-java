40910f4fb9a7c03d3521b4744fd7873eb59e9e66
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-09-12 23:24:07 +0530

    array (easy)

---
+++ 66. Plus One/Plus_One.java
@@ -0,0 +1,40 @@
+//time complexity O(n)
+//space complexity O(n)
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Plus_One {
+
+    private static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("enter the size of array ->");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        plusOne(arr);
+
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
+}
