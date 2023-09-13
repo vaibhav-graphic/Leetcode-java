aae989eb87453156bd410b6a9658347c5c6643e7
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-09-13 11:03:08 +0530

    Array Sorting (easy)

--- 747. Largest Number At Least Twice of Others/Number_At_Least_Twice_of_Others.java
+++ 747. Largest Number At Least Twice of Others/Number_At_Least_Twice_of_Others.java
@@ -1,31 +1,30 @@
 class Number_At_Least_Twice_of_Others {
 
-    private static int dominantIndex(int[] nums){
-        int max=Integer.MIN_VALUE;
-        int res=0;
+    private static int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int res = 0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>max){
-                max=nums[i];
-                res=i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                res = i;
             }
         }
 
-        nums[res]=0;
-        for(int i=0;i<nums.length;i++){
-            int check=0;
+        nums[res] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int check = 0;
 
-            check=nums[i]*2;
+            check = nums[i] * 2;
 
-            if(check>max)
+            if (check > max)
                 return -1;
-             
-                
         }
         return res;
     }
-    public static void main(String [] args){
-        int[] arr={3,6,1,0};
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 6, 1, 0 };
 
         System.out.println(dominantIndex(arr));
     }
