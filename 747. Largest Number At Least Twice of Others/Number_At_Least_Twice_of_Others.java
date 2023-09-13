657faf16876b1a77f231adf4d6c93062207bd901
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-09-13 09:57:42 +0530

    Array Sortinh (easy)

---
+++ 747. Largest Number At Least Twice of Others/Number_At_Least_Twice_of_Others.java
@@ -0,0 +1,32 @@
+class Number_At_Least_Twice_of_Others {
+
+    private static int dominantIndex(int[] nums){
+        int max=Integer.MIN_VALUE;
+        int res=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+                res=i;
+            }
+        }
+
+        nums[res]=0;
+        for(int i=0;i<nums.length;i++){
+            int check=0;
+
+            check=nums[i]*2;
+
+            if(check>max)
+                return -1;
+             
+                
+        }
+        return res;
+    }
+    public static void main(String [] args){
+        int[] arr={3,6,1,0};
+
+        System.out.println(dominantIndex(arr));
+    }
+}
