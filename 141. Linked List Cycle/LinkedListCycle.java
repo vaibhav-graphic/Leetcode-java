337e8695fd112de24c1bb38853b8a18753811136
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-09-22 11:21:45 +0530

    Linked List (easy)

---
+++ 141. Linked List Cycle/LinkedListCycle.java
@@ -0,0 +1,88 @@
+public class LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int d) {
+            val = d;
+            next = null;
+        }
+    }
+
+    ListNode head;
+
+    public void insert(int newData) {
+        // check linked list is not empty
+        if (head == null) {
+            head = new ListNode(newData);
+            return;
+        }
+        // create new node
+        ListNode newNode = new ListNode(newData);
+        // new node next is null
+        // inserti in the end
+        newNode.next = null;
+
+        // temp pointer
+        ListNode temp = head;
+
+        // travel in the linked list
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+
+        return;
+    }
+
+    public void display(){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.val+" ");
+            current=current.next;
+        }
+    }
+
+    //leetcode function
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+
+        while(slow!=null && fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        LinkedListCycle llist=new LinkedListCycle();
+        llist.insert(1);
+        llist.insert(2);
+        llist.insert(3);
+        llist.insert(4);
+        llist.insert(5);
+        llist.insert(6);
+        //llist.display();
+
+        //make circular list
+
+        ListNode temp=llist.head;
+
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=llist.head;
+
+        boolean res=llist.hasCycle(llist.head);
+        System.out.print(res);
+    }
+}
