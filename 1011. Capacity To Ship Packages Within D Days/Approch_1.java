e274059e73e07c691161c621b8d63febc0f348b5
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-10-17 11:40:18 +0530

    Arrays, Binary search(medium)

---
+++ 1011. Capacity To Ship Packages Within D Days/Approch_1.java
@@ -0,0 +1,41 @@
+public class Approch_1 {
+    public static int findDays(int[] weights, int cap) {
+        int days = 1;
+        int load = 0;
+        int n = weights.length;
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1;
+                load = weights[i];
+            } else {
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    public static int leastWeightCapacity(int[] weights, int d) {
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int i = 0; i < weights.length; i++) {
+            high += weights[i];
+            low = Math.max(low, weights[i]);
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+            if (numberOfDays <= d) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] weights = {5, 4, 5, 2, 3, 4, 5, 6};
+        int d = 5;
+        int ans = leastWeightCapacity(weights, d);
+        System.out.println("The minimum capacity should be: " + ans);
+    }
+}
