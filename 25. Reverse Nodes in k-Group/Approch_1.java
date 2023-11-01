305dffaff09d56ba80798b3f398f95a041a92771
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2023-11-01 09:27:22 +0530

    Linked List, Recursion(Hard)

---
+++ 25. Reverse Nodes in k-Group/Approch_1.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Approch_1 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head.next==null || k==1) return head;
+
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+
+        ListNode curr=dummy,n=dummy,pre=dummy;
+
+        int count=0;
+        while(curr.next!=null){
+            count++;
+            curr=curr.next;
+        }
+
+        while(count>=k){
+            curr=pre.next;
+            n=curr.next;
+
+            for(int i=1;i<k;i++){
+                curr.next=n.next;
+                n.next=pre.next;
+                pre.next=n;
+                n=curr.next;
+            }
+            pre=curr;
+            count-=k;
+        }
+
+        return dummy.next;
+    }
+}
