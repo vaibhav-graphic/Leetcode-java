72147220ffddb7d94bcda84ba20282bcd48f6966
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-03-08 12:15:03 +0530

    Array HashTable Countitng

---
+++ 3005. Count Elements With Maximum Frequency/Problem.java
@@ -0,0 +1,24 @@
+class Problem {
+    public int maxFrequencyElements(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        int sum = 0;
+
+        int[] freq = new int[101];
+
+        for(int val : nums){
+            freq[ val ]++;
+
+            int count = freq[ val ];
+
+            if(count > max){
+                max = count;
+                sum = max;
+            }
+            else if(count == max){
+                sum += max;
+            }
+        }
+        return sum;
+    }
+}
