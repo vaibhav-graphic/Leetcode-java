ab4d31298e194d390de2f3fad0e1a1c95f0ded59
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-03-18 11:27:05 +0530

    Arrays Greedy Sorting

---
+++ 452. Minimum Number of Arrows to Burst Balloons/Problem.java
@@ -0,0 +1,17 @@
+class Problem {
+    public int findMinArrowShots(int[][] points) {
+         Arrays.sort(points, (a, b) -> Integer.compare(a[1],b[1]));
+ 
+         int prev = points[0][1];
+         int count = 1;
+ 
+         for(int i=1; i<points.length; i++){
+             if(prev >= points[i][0]){
+                 continue;
+             }
+             count++;
+             prev = points[i][1];
+         }
+         return count;     
+     }
+ }
