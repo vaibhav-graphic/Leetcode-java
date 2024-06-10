5e56b71c118c967e18411a30f087af25b442b3ad
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-06-10 08:03:48 +0530

    Arrays Sorting Countring Sorting

---
+++ 1051. Height Checker/Problem.java
@@ -0,0 +1,20 @@
+class Problem {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int count = 0;
+        int[] exp = new int[n];
+
+        for(int i=0; i<n; i++){
+            exp[i] = heights[i];
+        }
+
+        Arrays.sort(exp);
+
+        for(int i=0; i<n; i++){
+            if(exp[i] != heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
