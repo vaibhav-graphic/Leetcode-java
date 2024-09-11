58bad8d1c7512d69851e16f7324405aa5bf828ac
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-09-11 14:46:13 +0530

    Bit masking , Bitmanupalation, Math

---
+++ 2220. Minimum Bit Flips to Convert Number/Problem.java
@@ -0,0 +1,17 @@
+class Problem {
+    public int minBitFlips(int start, int goal) {
+        int count = 0;
+
+        while(start != goal){
+            
+            if((start & 1) != (goal & 1)){
+                count++;
+            }
+
+            start >>= 1;
+            goal >>= 1;
+        }
+
+        return count;
+    }
+}
