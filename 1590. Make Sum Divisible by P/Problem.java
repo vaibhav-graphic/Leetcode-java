9c91f892f65fda8624f19c1370a9e0d760b1db6a
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-10-03 17:50:55 +0530

    Array Hash Table Prefix Sum

---
+++ 1590. Make Sum Divisible by P/Problem.java
@@ -0,0 +1,35 @@
+class Problem {
+    public int minSubarray(int[] nums, int p) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int n = nums.length;
+        int sum = 0;
+
+        for(int val : nums){
+            sum = (sum + val) % p; 
+        }
+
+        int target = sum % p;
+
+        if(target == 0){
+            return 0;
+        }
+
+        hm.put(0, -1);
+        int curr = 0;
+        int minLen = n;
+
+        for(int i=0; i<n; i++){
+            curr = (curr + nums[i] ) % p;
+            
+            int remain = (curr - target + p) % p;
+            
+            if(hm.containsKey(remain)){
+                minLen = Math.min(minLen, i - hm.get(remain));
+            }
+
+            hm.put(curr, i);
+        }
+
+        return minLen == n ? -1 : minLen;
+    }
+}
