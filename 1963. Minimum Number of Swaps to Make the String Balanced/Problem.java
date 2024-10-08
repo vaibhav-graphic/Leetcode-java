3c40e012d69d5cffaa2a554625c2f4d08e66d34b
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-10-08 12:59:49 +0530

    Two Pointers String Stack Greedy

---
+++ 1963. Minimum Number of Swaps to Make the String Balanced/Problem.java
@@ -0,0 +1,16 @@
+class Problem {
+    public int minSwaps(String s) {
+        int size = 0;
+
+        for(char ch : s.toCharArray()){
+            if(ch == '['){
+                size++;
+            }
+            else if(size > 0){
+                size--;
+            }
+        }
+
+        return (size + 1) / 2;
+    }
+}
