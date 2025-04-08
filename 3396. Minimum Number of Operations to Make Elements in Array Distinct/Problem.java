ad07fa7c2d7cde1357d0915acbbd13798066043d
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-04-08 10:05:52 +0530

    Array HashTable

---
+++ 3396. Minimum Number of Operations to Make Elements in Array Distinct/Problem.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            numList.add(num);
+        }
+
+        int operations = 0;
+
+        while (!isDistinct(numList)) {
+            int removeCount = Math.min(3, numList.size());
+            for (int i = 0; i < removeCount; i++) {
+                numList.remove(0);
+            }
+            operations++;
+        }
+
+        return operations;
+    }
+    
+    private boolean isDistinct(List<Integer> nums) {
+        Set<Integer> uniqueSet = new HashSet<>(nums);
+        return uniqueSet.size() == nums.size();
+    }
+}
