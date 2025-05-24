234b39804cb77d31559e407502eed492c33a99aa
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-05-24 15:22:24 +0530

    Array String

---
+++ 2942. Find Words Containing Character/Problem.java
@@ -0,0 +1,11 @@
+class Problem {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
