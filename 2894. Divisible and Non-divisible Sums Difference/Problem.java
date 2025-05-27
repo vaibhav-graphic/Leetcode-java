3b25142e4e95bb14da2ab4f07569735511e26b68
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-05-27 15:39:22 +0530

    Math

---
+++ 2894. Divisible and Non-divisible Sums Difference/Problem.java
@@ -0,0 +1,8 @@
+class Problem {
+    public int differenceOfSums(int n, int m) {
+        int totalSum = n * (n + 1) / 2;
+        int divisibleCount = n / m;
+        int divisibleSum = m * divisibleCount * (divisibleCount + 1) / 2;
+        return totalSum - 2 * divisibleSum;
+    }
+}
