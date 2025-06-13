e01fe4da30514c443f65d0cfb7cd50c9cf3063c9
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-06-13 19:43:06 +0530

    Array Binary Search Greedy

---
+++ 2616. Minimize the Maximum Difference of Pairs/Problem.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimizeMax(int[] v, int p) {
+        Arrays.sort(v); 
+        int n = v.length;
+        int lo = -1, hi = (int)1e9 + 7;
+
+        while (lo < hi - 1) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0;
+
+            for (int i = 1; i < n; i++) {
+                if (v[i] - v[i - 1] <= mid) {
+                    count++;
+                    i++; 
+                }
+            }
+
+            if (count >= p) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+
+        return hi;
+    }
+}
