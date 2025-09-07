ed87603527fbb9a4dda0d4f77ec00a6d1117bd67
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-09-07 16:12:12 +0530

    Array Math Weekly Contest 169

---
+++ 1304. Find N Unique Integers Sum up to Zero/Problem.java
@@ -0,0 +1,10 @@
+class Problem {
+        public int[] sumZero(int n) {
+        int [] arr = new int[n];
+        for (int i = 1;i <= n/2;i++){
+            arr[i-1] = i;
+            arr[n-i] = -i;
+        }
+        return arr;
+    }
+}
