15243c5b267ad092e41ebb7da4ff10508b49caee
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-09-08 16:40:08 +0530

    Math

---
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers/Problem.java
@@ -0,0 +1,11 @@
+class Problem {
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            int j = n - i;
+            if (!String.valueOf(i).contains("0") && !String.valueOf(j).contains("0")) {
+                return new int[]{i, j};
+            }
+        }
+        return new int[0];
+    }
+}
