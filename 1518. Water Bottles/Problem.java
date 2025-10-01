157f7f521a1e0d9ca93a3c6b048bf0e5a93a06e7
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-10-01 10:08:34 +0530

    Math Simulation Weekly Contest 198

---
+++ 1518. Water Bottles/Problem.java
@@ -0,0 +1,10 @@
+class Problem {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            numBottles += empty / numExchange;
+            empty = empty % numExchange + empty / numExchange;
+        }
+        return numBottles;
+    }
+}
