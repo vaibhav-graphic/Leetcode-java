b31c89126dbb27cff36baed6afdbd1fce5959abc
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-11-22 21:28:55 +0530

    Array Math Biweekly Contest 133

---
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three/Problem.java
@@ -0,0 +1,13 @@
+class P {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] % 3 != 0){
+                res++;
+            }
+        }
+        return res;
+    }
+}
