9b2ace8db3fbdf8d55b523c35484d9a20b253815
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-01-20 18:33:49 +0530

    Array Bit Manipulation Biweekly Contest 141

---
+++ 3314. Construct the Minimum Bitwise Array I/Problem.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+
+        for(int i=0; i<n; i++){
+            ans[i] = getElement(nums.get(i));
+        }
+
+        return ans;
+    }
+
+    private int getElement(int data){
+        int num = 1;
+
+        while(num <= data){
+            if((num | (num + 1)) == data){
+                return num;
+            }
+            num++;
+        }
+
+        return -1;
+    }
+}
