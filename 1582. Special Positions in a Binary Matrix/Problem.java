462f3fd98eaddd5bbfddefea2bd3c66b44221f0c
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-03-05 17:35:08 +0530

    Mid Level String

--- 1582. Special Positions in a Binary Matrix/Problem.java
+++ 1582. Special Positions in a Binary Matrix/Problem.java
@@ -1,4 +1,4 @@
-class Solution {
+class Problem {
     public int numSpecial(int[][] mat) {
 
         int specials = 0;
---
+++ 1758. Minimum Changes To Make Alternating Binary String/Problem.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(String s) {
+        
+        int n = s.length();
+        int start_with_0 = 0;//01010101
+        
+        for(int i=0; i<n; i++){
+            
+            if(i % 2 == 0){
+                if(s.charAt(i) == '1'){
+                    start_with_0++;
+                }
+            }else{
+                if(s.charAt(i) == '0'){
+                    start_with_0++;
+                }
+            }
+        }
+
+        int start_with_1 = n - start_with_0;
+        return Math.min(start_with_1,start_with_0);
+    }
+}
