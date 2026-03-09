fc8779772cdce6d529fe535c5b04697d0a5ef4db
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-03-09 22:53:33 +0530

    Staff

--- 1980. Find Unique Binary String/Problem1.java
+++ 1980. Find Unique Binary String/Problem1.java
@@ -1,4 +1,4 @@
-class Probelm {
+class Probelm1 {
     public String findDifferentBinaryString(String[] nums) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < nums.length; i++) {
