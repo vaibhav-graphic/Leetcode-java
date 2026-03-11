c5cf56f17b22cbd23b9b957e91cea0f29ca64207
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-03-11 20:58:46 +0530

    Junior Bit Manipulation Weekly Contest 128

---
+++ 1009. Complement of Base 10 Integer/Problem.java
@@ -0,0 +1,15 @@
+class SolutOion {
+    public int bitwiseComplement(int n) {
+        if(n==0) return 1; 
+        int count=0, t=n;
+
+        while(t>0){
+            t>>=1;
+            count++;
+        }
+        
+        int mask=(1<<count)-1;
+
+        return n^mask;
+    }
+}
