6cc0a0a2788314d757cbb2fe49df334d7c44ab0a
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-03-30 19:10:10 +0530

    Senior Hash Table String Sorting Biweekly Contest 112

--- 2839. Check if Strings Can be Made Equal With Operations I/Problem.java
+++ 2839. Check if Strings Can be Made Equal With Operations I/Problem.java
@@ -1,4 +1,4 @@
-class P {
+class Problem {
     public boolean canBeEqual(String s1, String s2) {
         return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
                 (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
