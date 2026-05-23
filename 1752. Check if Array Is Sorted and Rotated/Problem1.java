76c178bad711a49415f1c166a33f43347a15a594
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-05-23 21:32:51 +0530

    Mid Level Array Weekly Contest 227

---
+++ 1752. Check if Array Is Sorted and Rotated/Problem1.java
@@ -0,0 +1,17 @@
+class Problem {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] > nums[ (i+1) % n]){
+                count++;
+            }
+        }
+        
+        if(count <= 1){
+            return true;
+        }
+        return false;
+    }
+}
