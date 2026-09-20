7c6fcaa9f4da8feae56e6b333575c1da7f2727d5
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2026-09-20 12:23:48 +0530

    Mid Level String Simulation Biweekly Contest 153

--- 1401. Circle and Rectangle Overlapping/Problem.java
+++ 1401. Circle and Rectangle Overlapping/Problem.java
@@ -1,4 +1,4 @@
-class Pro {
+class Problem {
     public boolean checkOverlap(int r, int cx, int cy, int x1, int y1, int x2, int y2) {
         int x = Math.max(x1, Math.min(cx, x2)) - cx;
         int y = Math.max(y1, Math.min(cy, y2)) - cy;
